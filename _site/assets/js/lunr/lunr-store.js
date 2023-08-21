464dc6fe5cd6430a20a528fd21cbf49688a02947
Drew King <andrew.phillip.king@gmail.com>
2023-08-21 09:55:34 -0700

    Rebuilt site using

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "SuperLearner Screens",
+        "excerpt":"Investigating the use of screens in Super Learner ensembles Andrew King1; Brian Williamson, PhD2; and Ying Huang, PhD2 1Seattle Central College &amp; 2Fred Hutchinson Cancer Research Center Abstract Clinical research trials often generate “big data” Examples: vaccine and cancer early detection research Large number of participants = good Large number...","categories": [],
+        "tags": ["machine learning","data science","data visualization"],
+        "url": "/sl_screens/",
+        "teaser": null
+      },{
+        "title": "Solala App",
+        "excerpt":"Building a data driven Mental Health Application that using Node.js on Azure Lee Janzen; Christopher Roy; Christoph Bendix; and Drew King Abstract Current mental health apps fail in several manners: User retention, 80% of users uninstall after first month Large number of participants = good Attrition -&gt; challenging Goal is...","categories": [],
+        "tags": ["mental health app","software development","web development","mobile development"],
+        "url": "/solala/",
+        "teaser": null
+      },{
+        "title": "Allen School",
+        "excerpt":"Allen School Biography I’m a senior Computer Science student at the University of Washington with an expected graduation date of Fall 2024. Additionally I have been a teachers assistant for the Fall 2022 offering of CSE 390Z. At the moment, I am a Student Relations Officer for Ability. Studying and...","categories": [],
+        "tags": ["education","meta-cognition","accessibility"],
+        "url": "/allen_school/",
+        "teaser": null
+      }]
